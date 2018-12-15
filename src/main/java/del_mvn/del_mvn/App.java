1114261922414a175bb1938507d7067bca3803b8
techpiku <techpiku@gmail.com>
2018-12-16 02:07:59 +0530

    Code committed and pushed by techpiku@gmail.com on 16-Dec-2018 02:07 AM using DDLAB Gitpusher tool

---
+++ src/main/java/del_mvn/del_mvn/App.java
@@ -0,0 +1,13 @@
+package del_mvn.del_mvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
